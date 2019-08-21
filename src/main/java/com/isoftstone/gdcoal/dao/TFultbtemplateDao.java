7e1b359931d5fdfdfbb53e11ae10de68676e78b9
涂萍萍 <tpp_will@163.com>
2019-08-21 16:33:46 +0800

    dao

--- src/main/java/com/isoftstone/gdcoal/dao/TFultbtemplateDao.java
+++ src/main/java/com/isoftstone/gdcoal/dao/TFultbtemplateDao.java
@@ -3,14 +3,11 @@ package com.isoftstone.gdcoal.dao;
 
 import com.isoftstone.gdcoal.entity.TFultbtemplateEntity;
 import com.isoftstone.gdcoal.framework.dao.BaseDao;
-
-import java.util.List;
-import java.util.Map;
-
+import com.isoftstone.gdcoal.framework.entity.BaseEntity;
 
 /**
  * Created by issuser on 2019/7/3.
  */
 public interface TFultbtemplateDao extends BaseDao<TFultbtemplateEntity> {
-     public List<Map<String,Object>> selectPageTFultbtemplate(TFultbtemplateEntity entity);
+
 }
